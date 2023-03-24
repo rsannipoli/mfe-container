7d5e67aafbb1978076db179f41c4566386a0dfa5
Riccardo <r.sannipoli@lineargruppo.it>
2023-03-24 12:05:52 +0100

    change project folder name

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,26 +1,21 @@
-// Merge common configuration with the current development config
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+    output: {
+        filename: '[name].[contentHash].js'
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+            },
+            shared: packageJson.dependencies
         }),
     ]
 }
