0cbb6b84fbcc52052964cad330b68ed199daf556
Riccardo <r.sannipoli@lineargruppo.it>
2023-03-24 10:58:11 +0100

    first commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,30 @@
+// Merge common configuration with the current development config
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: "marketing@http://localhost:8081/remoteEntry.js"
+            },
+            shared: packageJson.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return <>
+        <h1>component</h1>
+        <hr />
+        <MarketingApp />
+    </>;
+}
